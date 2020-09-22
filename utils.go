ed9096179be678eaf3eaf94bdc9cbb4a453e4e69
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-22 11:28:31 +0300

    Handle all basic types in function arguments

--- utils.go
+++ utils.go
@@ -13,10 +13,46 @@ func isReference(t reflect.Type) bool {
 	return t.Kind() == reflect.Ptr
 }
 
-// TODO add all primitive types
+// Handle all primitive (basic) types
 func isPrimitive(str string) bool {
 	switch str {
+	case "bool":
+		return true
+	case "string":
+		return true
 	case "int":
+		return true
+	case "int8":
+		return true
+	case "int16":
+		return true
+	case "int32":
+		return true
+	case "int64":
+		return true
+	case "uint":
+		return true
+	case "uint8":
+		return true
+	case "uint16":
+		return true
+	case "uint32":
+		return true
+	case "uint64":
+		return true
+	case "uintptr":
+		return true
+	case "byte":
+		return true
+	case "rune":
+		return true
+	case "float32":
+		return true
+	case "float64":
+		return true
+	case "complex64":
+		return true
+	case "complex128":
 		return true
 	default:
 		return false
