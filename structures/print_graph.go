ac0cb28f55e880c43bafea64222de3abe36ac32c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-10-24 19:57:32 +0300

    Update graph printing for windows

--- structures/print_graph.go
+++ structures/print_graph.go
@@ -6,13 +6,15 @@ import (
 	"bytes"
 
 	"github.com/goccy/go-graphviz"
+	"github.com/spiral/endure/errors"
 )
 
 func PrintGraph(vertices []*Vertex) error {
+	const op = errors.Op("print_graph")
 	gr := graphviz.New()
 	graph, err := gr.Graph()
 	if err != nil {
-		panic(err)
+		return errors.E(op, err)
 	}
 
 	for i := 0; i < len(vertices); i++ {
@@ -20,17 +22,17 @@ func PrintGraph(vertices []*Vertex) error {
 			for j := 0; j < len(vertices[i].Dependencies); j++ {
 				n, err := graph.CreateNode(vertices[i].ID)
 				if err != nil {
-					return err
+					return errors.E(op, err)
 				}
 
 				m, err := graph.CreateNode(vertices[i].Dependencies[j].ID)
 				if err != nil {
-					return err
+					return errors.E(op, err)
 				}
 
 				e, err := graph.CreateEdge("", n, m)
 				if err != nil {
-					return err
+					return errors.E(op, err)
 				}
 				e.SetLabel("")
 			}
@@ -39,12 +41,12 @@ func PrintGraph(vertices []*Vertex) error {
 
 	var buf bytes.Buffer
 	if err := gr.Render(graph, graphviz.PNG, &buf); err != nil {
-		return err
+		return errors.E(op, err)
 	}
 
 	// write to file directly
 	if err := gr.RenderFilename(graph, graphviz.PNG, "./graph.png"); err != nil {
-		return err
+		return errors.E(op, err)
 	}
 	return nil
 }
--- structures/print_graph_windows.go
+++ structures/print_graph_windows.go
@@ -1,49 +1,12 @@
 // +build windows
+
 package structures
 
 import (
-	"bytes"
-
-	"github.com/goccy/go-graphviz"
+	"github.com/spiral/endure/errors"
 )
 
 func PrintGraph(vertices []*Vertex) error {
-	gr := graphviz.New()
-	graph, err := gr.Graph()
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < len(vertices); i++ {
-		if len(vertices[i].Dependencies) > 0 {
-			for j := 0; j < len(vertices[i].Dependencies); j++ {
-				n, err := graph.CreateNode(vertices[i].ID)
-				if err != nil {
-					return err
-				}
-
-				m, err := graph.CreateNode(vertices[i].Dependencies[j].ID)
-				if err != nil {
-					return err
-				}
-
-				e, err := graph.CreateEdge("", n, m)
-				if err != nil {
-					return err
-				}
-				e.SetLabel("")
-			}
-		}
-	}
-
-	var buf bytes.Buffer
-	if err := gr.Render(graph, graphviz.PNG, &buf); err != nil {
-		return err
-	}
-
-	// write to file directly
-	if err := gr.RenderFilename(graph, graphviz.PNG, "./graph.png"); err != nil {
-		return err
-	}
-	return nil
+	const op = errors.Op("print_graph")
+	return errors.E(op, errors.Unsupported, errors.Str("windows currently not supported for this feature"))
 }
